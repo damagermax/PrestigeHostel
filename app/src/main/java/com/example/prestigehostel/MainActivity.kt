2aee1d3a5fbbbfb4d9997106500c13c84d01241e
Maxwell Takyi <72990512+damagermax@users.noreply.github.com>
2022-04-21 15:11:57 +0000

    Initial commit

---
+++ app/src/main/java/com/example/prestigehostel/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.prestigehostel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/*
+ * created by maxwell takyi on 21 april 2022
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
